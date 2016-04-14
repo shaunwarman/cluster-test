2b302a98102ca67ebdfe5484c419c4bbae467d64
Shaun Warman <swarman@paypal.com>
2016-04-14 16:16:32 -0500

    Add unique port for each worker to listen on

--- index.js
+++ index.js
@@ -8,11 +8,17 @@ if (cluster.isMaster) {
     console.log(`Master cluster setting up ${numWorkers} workers...`);
 
     for (var i = 0; i < numWorkers; i++) {
-        cluster.fork();
+        let port = 8000 + i;
+        cluster.fork({port: port});
     }
 
     cluster.on('online', (worker) => {
         console.log(`Worker ${worker.process.pid} is online`);
+    });
+
+    cluster.on('listening', (worker, address) => {
+        console.log(
+            `A worker is now connected to ${address.address}:${address.port}`);
     });
 
     cluster.on('exit', (worker, code, signal) => {
@@ -28,20 +34,24 @@ if (cluster.isMaster) {
         res.send(`process ${process.pid} says hello!`).end();
     });
 
-    const server = app.listen(8000, () => {
-        console.log(`Process ${process.pid} is listening to all incoming requests`);
+    const server = app.listen(process.env.port, () => {
+        console.log(`Process ${process.pid} is listening to all incoming requests on port ${process.env.port}`);
     });
 
-    // kill the process after random time between 5 and 15 sec
+    sendKill();
+}
+
+// kill the process after random time between 5 and 15 sec
+function sendKill() {
     setTimeout(() => {
         console.log(`Killing process ${process.pid}`);
         throw new Error('Randomly killing process!');
-    }, Math.random()*10000+5000);
+    }, Math.random() * 10000 + 5000);
 }
 
 // catch all uncaughtExceptions
 process.on('uncaughtException', (err) => {
-    console.error(`${(new Date).toUTCString()} uncaughtException: ${err.message}`);
-    console.error(err.stack);
+    console.log(`${(new Date).toUTCString()} uncaughtException: ${err.message}`);
+    console.log(err.stack);
     process.exit(1);
 });
